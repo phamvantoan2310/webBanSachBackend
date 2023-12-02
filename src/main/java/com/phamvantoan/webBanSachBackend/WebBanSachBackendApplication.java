894487c70f8a923ff25e2772c895a72dbec28d85
Pham Van Toan <phamvantoan231003@gmail.com>
2023-12-02 08:57:58 +0700

    generate project

---
+++ src/main/java/com/phamvantoan/webBanSachBackend/WebBanSachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phamvantoan.webBanSachBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachBackendApplication.class, args);
+	}
+
+}
